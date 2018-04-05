e086f6ebd2f80d70fc74190f5555a732ff90e42c
geordyp <geowilliams6@gmail.com>
2018-04-05 16:05:26 -0500

    Add error messages

--- mysite/styletransfer/views.py
+++ mysite/styletransfer/views.py
@@ -17,39 +17,53 @@ def FormView(request):
     """
     Displays the form for user input
     """
-    return render(request, 'styletransfer/form.html')
+    if request.method == 'POST':
+        if 'styleOption' not in request.POST:
+            return render(request,
+                          'styletransfer/form.html',
+                          {
+                            'errorStyle':"Please select a style"
+                          },
+                          status=400)
+        else:
+            request.session['styleSelected'] = request.POST['styleOption']
+
+        if 'snapShot' in request.POST and request.POST['snapShot'] != "":
+            imageData = re.search(r'base64,(.*)', request.POST['snapShot']).group(1)
+            userImage = open('static/styletransfer/images/userImage.png', 'wb')
+            userImage.write(base64.b64decode(imageData))
+            userImage.close()
+        elif 'fileUpload' in request.FILES and request.FILES['fileUpload'] is not None:
+            userImage = open('static/styletransfer/images/userImage.png', 'wb')
+            userImage.writelines(request.FILES['fileUpload'].readlines())
+            userImage.close()
+        else:
+            return render(request,
+                          'styletransfer/form.html',
+                          {
+                            'errorImage':"Please take a picture or upload an image"
+                          },
+                          status=400)
+
+        #########################
+        # TODO run style transfer, create result file
+        #########################
+
+        return HttpResponseRedirect(reverse('views.result', args=()))
+    else:
+        return render(request, 'styletransfer/form.html')
 
 
 def ResultView(request):
     """
     Displays the result of the style transfer
     """
-    if request.method == 'POST':
-        if 'styleOption' not in request.POST:
-            # TODO add error message
-            return HttpResponseRedirect(reverse('views.form', args=()))
-
-        if 'snapshot' in request.POST and request.POST['snapshot'] != "":
-            imageData = re.search(r'base64,(.*)', request.POST['snapshot']).group(1)
-            userImage = open('static/styletransfer/images/userImage.png', 'wb')
-            userImage.write(base64.b64decode(imageData))
-            userImage.close()
-        elif 'fileupload' in request.FILES and request.FILES['fileupload'] is not None:
-            userImage = open('static/styletransfer/images/userImage.png', 'wb')
-            userImage.writelines(request.FILES['fileupload'].readlines())
-            userImage.close()
-        else:
-            # TODO add error message
-            return HttpResponseRedirect(reverse('views.form', args=()))
-
-        #########################
-        # TODO run style transfer
-        #########################
-
+    # if there is a userImage, styleSelection, and resultImage
+    if 'styleSelected' in request.session:
         return render(request,
                       'styletransfer/result.html',
                       {
-                        'styleSelection':request.POST['styleOption'],
+                        'styleSelection':request.session['styleSelected'],
                         'result':'http://via.placeholder.com/200x280'
                       })
     else:
