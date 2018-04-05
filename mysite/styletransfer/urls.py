14a7893626797b413f302f03a219bc89e80e61fd
geordyp <geowilliams6@gmail.com>
2018-04-04 22:11:12 -0500

    Create minimal user flow

--- mysite/styletransfer/urls.py
+++ mysite/styletransfer/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView, name='index'),
+    path('', views.IndexView, name='styletransfer.views.index'),
+    path('form/', views.FormView, name='styletransfer.views.form'),
+    path('result/', views.ResultView, name='styletransfer.views.result'),
 ]
--- mysite/styletransfer/views.py
+++ mysite/styletransfer/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def IndexView(request):
+    """
+    Tells the user about the project and how it works
+    """
     return render(request, 'styletransfer/index.html')
+
+
+def FormView(request):
+    """
+    Gets the selfie from the user
+    """
+    # display form
+    return render(request, 'styletransfer/form.html')
+
+
+def ResultView(request):
+    """
+    Displays the result of the style transfer
+    """
+    # receive inputs and run style transfer
+    # display results
+    return render(request, 'styletransfer/result.html')
