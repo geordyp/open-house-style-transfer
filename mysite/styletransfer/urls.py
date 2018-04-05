2cac325750d4f04c581dbaf55212a9a546e7d045
geordyp <geowilliams6@gmail.com>
2018-04-05 14:20:52 -0500

    Capture userImage and store

--- mysite/styletransfer/urls.py
+++ mysite/styletransfer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView, name='styletransfer.views.index'),
-    path('form/', views.FormView, name='styletransfer.views.form'),
-    path('result/', views.ResultView, name='styletransfer.views.result'),
+    path('', views.IndexView, name='views.index'),
+    path('form/', views.FormView, name='views.form'),
+    path('result/', views.ResultView, name='views.result'),
 ]
--- mysite/styletransfer/views.py
+++ mysite/styletransfer/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+
+import re
+import base64
 
 
 def IndexView(request):
@@ -11,9 +15,8 @@ def IndexView(request):
 
 def FormView(request):
     """
-    Gets the selfie from the user
+    Displays the form for user input
     """
-    # display form
     return render(request, 'styletransfer/form.html')
 
 
@@ -21,6 +24,24 @@ def ResultView(request):
     """
     Displays the result of the style transfer
     """
-    # receive inputs and run style transfer
-    # display results
-    return render(request, 'styletransfer/result.html')
+    if request.method == "POST":
+        # TODO further validate input
+        if "styleOption" not in request.POST or "snapshot" not in request.POST and "fileUpload" not in request.POST:
+           return HttpResponseRedirect(reverse('views.form', args=()))
+
+        imgstr = re.search(r'base64,(.*)', request.POST['snapshot']).group(1)
+        output = open('static/styletransfer/images/userImage.png', 'wb')
+        output.write(base64.b64decode(imgstr))
+        output.close()
+
+        # TODO run style transfer
+
+        # userImage = request.POST['snapshot'] if "snapshot" in request.POST else request.POST['fileUpload']
+        return render(request,
+                      'styletransfer/result.html',
+                      {
+                        'styleSelection':request.POST['styleOption'],
+                        'result':'http://via.placeholder.com/200x280'
+                      })
+    else:
+        return HttpResponseRedirect(reverse('views.form', args=()))
